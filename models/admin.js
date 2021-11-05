2e256149dad213d72af3378bfb85c55cdc6c781e
Kevin <kfig21@gmail.com>
2021-11-05 00:22:32 -0400

    first commit

---
+++ models/admin.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+
+const AdminSchema = new Schema({
+  username: { required: true, type: String },
+  password: { required: true, type: String },
+});
+
+AdminSchema.pre("save", async function (next) {
+  const hash = await bcrypt.hash(this.password, 10);
+
+  this.password = hash;
+  next();
+});
+
+AdminSchema.methods.isValidPassword = async function (password) {
+  const user = this;
+  const compare = await bcrypt.compare(password, user.password);
+
+  return compare;
+};
+
+const Admin = mongoose.model("Admin", AdminSchema);
+
+module.exports = Admin;
