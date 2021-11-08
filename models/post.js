31c275668279c0e55379b5429091ecac76a3d154
Kevin <kfig21@gmail.com>
2021-11-08 17:34:42 -0500

    update for comments

--- models/post.js
+++ models/post.js
@@ -7,7 +7,6 @@ const postSchema = new Schema({
   text: { required: true, type: String },
   author: { required: true, type: String },
   published: { default: false, type: Boolean },
-  comments: { type: Array },
 });
 
 postSchema.virtual("date_formated").get(function () {
