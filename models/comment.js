d8afcf3d28cf39e4a914516802574565179be10a
Kevin <kfig21@gmail.com>
2021-11-09 12:46:27 -0500

    update for comments

--- models/comment.js
+++ models/comment.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
 
 const commentSchema = new Schema({
   date: { type: Date, default: Date.now, required: true },
