001ab9508b5c3289645acaa281c957f0cecfba35
Kevin <kfig21@gmail.com>
2021-11-09 13:07:18 -0500

    update for comments

--- models/comment.js
+++ models/comment.js
@@ -2,17 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const { DateTime } = require("luxon");
 
-const commentSchema = new Schema({
+const CommentSchema = new Schema({
   date: { type: Date, default: Date.now, required: true },
   text: { required: true, type: String },
   user: { required: true, type: String },
-  postId: { type: Schema.Types.ObjectId, ref: "Post" },
+  postId: { type: String, required: true },
 });
 
-commentSchema.virtual("submitted").get(function () {
+CommentSchema.virtual("submitted").get(function () {
   return DateTime.fromJSDate(this.date).toLocaleString(DateTime.DATETIME_MED);
 });
 
-const Comment = mongoose.model("Comment", commentSchema);
-
-module.exports = Comment;
+module.exports = mongoose.model("Comment", CommentSchema);
