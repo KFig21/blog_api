fc198698b99af390c1d8a0e673a6d792aa083af2
Kevin <kfig21@gmail.com>
2021-11-09 20:35:24 -0500

    update for comments

--- models/post.js
+++ models/post.js
@@ -1,14 +1,21 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
 
-const PostSchema = new Schema({
-  title: { type: String, required: true },
-  date: { type: Date, default: Date.now, required: true },
-  text: { required: true, type: String },
-  author: { required: true, type: String },
-  published: { default: false, type: Boolean },
-  comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
-});
+const PostSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    date: { type: Date, default: Date.now, required: true },
+    text: { required: true, type: String },
+    author: { required: true, type: String },
+    published: { default: false, type: Boolean },
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 PostSchema.virtual("submitted").get(function () {
   return DateTime.fromJSDate(this.date).toLocaleString(DateTime.DATETIME_MED);
