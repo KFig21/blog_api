486725f6e050f6f861292e671aced8dd4da573a4
Kevin <kfig21@gmail.com>
2021-11-09 23:30:51 -0500

    update for comments

--- models/post.js
+++ models/post.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const { DateTime } = require("luxon");
+const slugify = require("slugify");
+const marked = require("marked");
+const createDomPurify = require("dompurify");
+const { JSDOM } = require("jsdom");
+const dompurify = createDomPurify(new JSDOM().window);
 
 const PostSchema = new Schema(
   {
@@ -10,12 +15,28 @@ const PostSchema = new Schema(
     author: { required: true, type: String },
     published: { default: false, type: Boolean },
     comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+    slug: { type: String, required: true, unique: true },
+    sanitizedHtml: { type: String, required: true },
   },
   {
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
   }
 );
+
+PostSchema.pre("validate", function (next) {
+  // create slug for url using post title
+  if (this.title) {
+    this.slug = slugify(this.title, { lower: true, strict: true });
+  }
+
+  // sanitize post so it cannot be hacked
+  if (this.text) {
+    this.sanitizedHtml = dompurify.sanitize(marked(this.text));
+  }
+
+  next();
+});
 
 PostSchema.virtual("submitted").get(function () {
   return DateTime.fromJSDate(this.date).toLocaleString(DateTime.DATETIME_MED);
