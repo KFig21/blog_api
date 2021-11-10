0f8c4a29f6b445bc9c264668a2a48fc3cf51d72d
Kevin <kfig21@gmail.com>
2021-11-09 20:34:57 -0500

    update for comments

--- models/post.js
+++ models/post.js
@@ -1,25 +1,17 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const postSchema = new Schema(
-  {
-    title: { type: String, required: true },
-    date: { type: Date, default: Date.now, required: true },
-    text: { required: true, type: String },
-    author: { required: true, type: String },
-    published: { default: false, type: Boolean },
-    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
-  },
-  {
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true },
-  }
-);
+const PostSchema = new Schema({
+  title: { type: String, required: true },
+  date: { type: Date, default: Date.now, required: true },
+  text: { required: true, type: String },
+  author: { required: true, type: String },
+  published: { default: false, type: Boolean },
+  comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+});
 
-postSchema.virtual("submitted").get(function () {
+PostSchema.virtual("submitted").get(function () {
   return DateTime.fromJSDate(this.date).toLocaleString(DateTime.DATETIME_MED);
 });
 
-const Post = mongoose.model("Post", postSchema);
-
-module.exports = Post;
+module.exports = mongoose.model("Post", PostSchema);
